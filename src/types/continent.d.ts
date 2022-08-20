462ab3c0adc05a03f1bd3482662213abef39a682
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-08-20 12:32:57 -0300

    :sparkles: feat: create continent type

---
+++ src/types/continent.d.ts
@@ -0,0 +1,26 @@
+type ContinentStatistics = {
+  id: string;
+  label: string;
+  quantity: number;
+  tooltipText?: string;
+};
+
+type MostVisitedCities = {
+  id: string;
+  image: string;
+  name: string;
+  country: string;
+  countryFlag: string;
+};
+
+export type Continent = {
+  slug: string;
+  name: string;
+  cta: string;
+  path: string;
+  slideImage: string;
+  detailBanner: string;
+  description: string;
+  statistics: ContinentStatistics[];
+  mostVisitedCities: MostVisitedCities[];
+};
